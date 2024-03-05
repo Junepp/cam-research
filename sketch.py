7a42fe4013988fae43b2e3591a651c273948e702
Junepp <jhjun1030@gmail.com>
2024-03-05 18:11:58 +0900

    Update: subo control

--- sketch.py
+++ sketch.py
@@ -3,20 +3,51 @@ import numpy as np
 from Detector import Detector
 from Calibrator import Calibrator
 
+
+def deltaDegreeToDutyRatio(deltaDegree: float):
+    7.5 + (deltaDegree / 36)
+    ...
+
+
 DEBUG = True
 W = 1920
 H = 1080
 
 det_instance = Detector()
-cal_instance = Calibrator(file_path='camera_intrinsics_c922pro.dat')
+cal_instance = Calibrator(file_path='cam_parameters/camera_intrinsics_c922pro.dat')
 
+# camera setting
 cam = cv2.VideoCapture(0)
-
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, W)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, H)
 
+# subo motor setting
+import time
+import RPI.GPIO as GPIO
+
+# set pin number
+pinAzim = 18
+pinElev = 19
+
+# pin number rule - GPIO.BCM or GPIO.BOARD
+GPIO.setmode(GPIO.BCM)
+
+# set output mode
+# GPIO.setup([pinAzim, pinElev], GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(pinAzim, GPIO.OUT)
+GPIO.setup(pinElev, GPIO.OUT)
+
+# set and initialize PWM
+pulseFreq = 50  # based on MG996R datasheet
+pulseAzim = GPIO.PWM(pinAzim, pulseFreq)
+pulseElev = GPIO.PWM(pinElev, pulseFreq)
+
+pulseAzim.start(7.5)  # 7.5 dutyRatio = 90 degree
+pulseElev.start(7.5)
+
 while cam.isOpened():
     status, frame = cam.read()
+    print(type(frame))
 
     if status:
         result = det_instance.predict(frame)
@@ -35,15 +66,28 @@ while cam.isOpened():
             elev = np.arctan(n2y) * 57.2958  # radian to degree
             print(f'azim: {azim:.1f}, elev: {elev:.1f}')
 
-            # TODO. generate PWM signal
-            # TODO. send PWM to each subo-motor
+            # generate and send PWM signal
+            azimDutyRatio = 7.5 + (azim / 36)
+            elevDutyRatio = 7.5 + (elev / 36)
 
-            if DEBUG:  # debug
+            pulseAzim.ChangeDuty(azimDutyRatio)
+            pulseElev.ChangeDuty(elevDutyRatio)
+
+            # TODO. set waiting time
+            # TODO. set limit of degree (0 to 180)
+            """
+            pulse frequency = 50hz
+            20ms per each pulse
+            1 ms ~ 0 degree (duty ratio = 5%)
+            2 ms ~ 180 degree (duty ratio = 10%) 
+            """
+
+            if DEBUG:
                 cv2.line(hud_frame, (bcx, 0), (bcx, H), color=(0, 0, 255), thickness=1)  # draw vertical line
                 cv2.line(hud_frame, (0, bcy), (W, bcy), color=(0, 0, 255), thickness=1)  # draw horizontal line
                 cv2.putText(hud_frame, f'azim: {azim:.1f}', (bcx, bcy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
 
-        if DEBUG:  # debug
+        if DEBUG:
             cv2.imshow("DEBUG", hud_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -54,3 +98,5 @@ else:
 
 cam.release()
 cv2.destroyAllWindows()
+
+GPIO.cleanup()
