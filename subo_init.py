719f660464efc20897c022a12f3bbf2b4e8d967f
Junepp <jhjun1030@gmail.com>
2024-03-24 13:31:11 +0900

    temp

--- subo_init.py
+++ subo_init.py
@@ -10,13 +10,14 @@ pulseFreq = 50  # based on MG996R datasheet
 servo = GPIO.PWM(servoPin, pulseFreq)
 
 if __name__ == "__main__":
-    servo.start(0)  # 7.5 dutyRatio = 90 degree
-
+    servo.start(7.5)  # 7.5 dutyRatio = 90 degree
     time.sleep(3)
+
     servo.ChangeDutyCycle(5)
-
     time.sleep(3)
+
     servo.ChangeDutyCycle(10)
-
     time.sleep(3)
+
     servo.ChangeDutyCycle(7.5)
+    time.sleep(3)
