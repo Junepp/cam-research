8e67014c92eb984a19b58443732c9d03a89e0502
Junepp <jhjun1030@gmail.com>
2024-03-24 13:25:43 +0900

    temp

--- subo_init.py
+++ subo_init.py
@@ -6,16 +6,15 @@ except RuntimeError:
     print('not exists')
 
 
-pinAzim = 18
+pinAzim = 12
 
-# GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pinAzim, GPIO.OUT)
 
 pulseFreq = 50  # based on MG996R datasheet
 pulseAzim = GPIO.PWM(pinAzim, pulseFreq)
 
-pulseAzim.start(7.5)  # 7.5 dutyRatio = 90 degree
+pulseAzim.start(0)  # 7.5 dutyRatio = 90 degree
 
 time.sleep(3)
 pulseAzim.ChangeDutyCycle(5)
