32196e74fc914fa430de691689cee77cee52d983
Junepp <jhjun1030@gmail.com>
2024-03-24 13:45:57 +0900

    temp

--- subo_init.py
+++ subo_init.py
@@ -1,23 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPin = 12
+servoAzimPin = 12
+servoElevPin = 11
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPin, GPIO.OUT)
+GPIO.setup(servoAzimPin, GPIO.OUT)
+GPIO.setup(servoElevPinv, GPIO.OUT)
+
 
 pulseFreq = 50  # based on MG996R datasheet
-servo = GPIO.PWM(servoPin, pulseFreq)
+servoAzim = GPIO.PWM(servoAzimPin, pulseFreq)
+servoElev = GPIO.PWM(servoElevPin, pulseFreq)
 
 if __name__ == "__main__":
-    servo.start(7.5)  # 7.5 dutyRatio = 90 degree
+    servoAzim.start(7.5)  # 7.5 dutyRatio = 90 degree
+    servoElev.start(7.5)  # 7.5 dutyRatio = 90 degree
     time.sleep(3)
 
-    servo.ChangeDutyCycle(2.5)
+    servoAzim.ChangeDutyCycle(2.5)
+    servoElev.ChangeDutyCycle(2.5)
     time.sleep(3)
 
-    servo.ChangeDutyCycle(12.5)
+    servoAzim.ChangeDutyCycle(12.5)
+    servoElev.ChangeDutyCycle(12.5)  # 7.5 dutyRatio = 90 degree
     time.sleep(3)
 
-    servo.ChangeDutyCycle(7.5)
+    servoAzim.ChangeDutyCycle(7.5)
+    servoElev.ChangeDutyCycle(7.5)  # 7.5 dutyRatio = 90 degree
     time.sleep(3)
