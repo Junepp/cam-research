bcad12ab4087ab260dda67e12745c64077a4a862
Junepp <jhjun1030@gmail.com>
2024-03-24 13:29:23 +0900

    temp

--- subo_init.py
+++ subo_init.py
@@ -1,22 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-pinAzim = 12
+servoPin = 12
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pinAzim, GPIO.OUT)
+GPIO.setup(servoPin, GPIO.OUT)
 
 pulseFreq = 50  # based on MG996R datasheet
-pulseAzim = GPIO.PWM(pinAzim, pulseFreq)
+servo = GPIO.PWM(servoPin, pulseFreq)
 
 if __name__ == "__main__":
-    pulseAzim.start(0)  # 7.5 dutyRatio = 90 degree
+    servo.start(0)  # 7.5 dutyRatio = 90 degree
 
-    time.sleep(3)
-    pulseAzim.ChangeDutyCycle(5)
+    time.sleep(1)
+    servo.ChangeDutyCycle(5)
 
-    time.sleep(3)
-    pulseAzim.ChangeDutyCycle(10)
+    time.sleep(1)
+    servo.ChangeDutyCycle(10)
 
-    time.sleep(3)
-    pulseAzim.ChangeDutyCycle(7.5)
+    time.sleep(1)
+    servo.ChangeDutyCycle(7.5)
