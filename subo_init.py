04f687b6009ffc81f2e5cd371dbe9888b9b09db3
Junepp <jhjun1030@gmail.com>
2024-03-24 12:56:26 +0900

    test

--- subo_init.py
+++ subo_init.py
@@ -1,5 +1,26 @@
+import time
 try:
     import RPi.GPIO as GPIO
 
 except RuntimeError:
     print('not exists')
+
+
+pinAzim = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pinAzim, GPIO.OUT)
+
+pulseFreq = 50  # based on MG996R datasheet
+pulseAzim = GPIO.PWM(pinAzim, pulseFreq)
+
+pulseAzim.start(7.5)  # 7.5 dutyRatio = 90 degree
+
+time.sleep(5)
+pulseAzim.start(5)  # 7.5 dutyRatio = 90 degree
+
+time.sleep(5)
+pulseAzim.start(10)  # 7.5 dutyRatio = 90 degree
+
+time.sleep(5)
+pulseAzim.start(7.5)  # 7.5 dutyRatio = 90 degree
